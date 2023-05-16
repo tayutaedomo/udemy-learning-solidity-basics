ec3d52bde9431fc3993c77c189cbb6bba6f9d2d9
tayutaedomo <tayutaedomo@gmail.com>
2023-05-16 21:55:28 +0900

    FunctionCall.sol: 実装を追加

--- contracts/Section03/practice/FunctionCall.sol
+++ contracts/Section03/practice/FunctionCall.sol
@@ -12,20 +12,52 @@ pragma solidity ^0.8.17;
  */
 contract FunctionCall {
     /// @dev Publicファンクションは外部からCallできる
+    function pub1(uint a, uint b) public pure returns (uint) {
+        return _itl1(a, b);
+    }
 
     /// @dev Internalファンクションは外部からはCallできない。内部からのみCallできる
+    function _itl1(uint a, uint b) internal pure returns (uint) {
+        return (a + b);
+    }
 
     /// @dev External Function Call
+    function ext1(uint a, uint b) external pure returns (uint) {
+        return (a + b);
+    }
 
     /// @dev PublicファンクションからPrivateファンクションをCallできる
+    function pub2(uint a, uint b) public pure returns (uint) {
+        return _pri2(a, b);
+    }
+
+    function pub3(uint a, uint b) public pure returns (uint) {
+        return (a * b);
+    }
 
     /// @dev PrivateファンクションからPublicファンクションをCallできる
+    function _pri2(uint a, uint b) private pure returns (uint) {
+        return pub3(a, b);
+    }
 
     /// @dev ExternalファンクションはInternalファンクションからCallできない
+    function _itl3(uint a, uint b) internal view returns (uint) {
+        //return ext1(a, b);
+        return this.ext1(a, b);
+    }
 
+    function pub4(uint a, uint b) public view returns (uint) {
+        return _itl3(a, b);
+    }
 
     /// @dev 引数の指定
+    function _itl4(uint a, uint b) internal pure returns (uint) {
+        return (a ** b);
+    }
 
     //未使用の仮引数は名前を省略できる
-
+    function pub5(uint a_, uint b_, uint) public pure returns (uint) {
+        //return _itl4({b: b_, a: a_});
+        return _itl4(b_, a_);
+    }
 }
