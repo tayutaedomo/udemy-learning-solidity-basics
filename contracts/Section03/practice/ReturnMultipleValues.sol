ff1a2c94bca2006a58d006a18ed8a87c9f10fb37
tayutaedomo <tayutaedomo@gmail.com>
2023-05-17 02:00:27 +0900

    ReturnMultipleValues.sol: 実装を追加

--- contracts/Section03/practice/ReturnMultipleValues.sol
+++ contracts/Section03/practice/ReturnMultipleValues.sol
@@ -7,15 +7,28 @@ pragma solidity ^0.8.17;
  */
 contract ReturningMultipleValues {
     // 複数の戻りを指定
-
+    function multipleReturn() internal pure returns (uint, string memory, bool) {
+        return (1, "foo", true);
+    }
 
     // ファンクションの複数の戻り値を受け取る
-
+    function callMultipleReturn() public pure returns (uint, string memory, bool) {
+        (uint a, string memory b, bool c) = multipleReturn();
+        return (a, b, c);
+    }
 
     // ファンクションの複数の戻り値から必要なものだけ抽出
-
+    function throwAwayReturnValue() public pure returns (uint, bool) {
+        (uint a, , bool c) = multipleReturn();
+        return (a, c);
+    }
 
     /// @dev Function return variables（関数の戻り値変数）を利用した複数return
+    function add(uint a_, uint b_) public pure returns (uint mulNum, uint sumNum) {
+        sumNum = a_ + b_;
+        mulNum = a_ * b_;
+        return (mulNum, sumNum);
+    }
 
         /// @dev returnのためだけに使う変数は型の定義不要
 
