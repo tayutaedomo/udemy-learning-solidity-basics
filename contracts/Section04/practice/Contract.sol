db77c4bebfee7df47d5f2e322a1604c04ba205fe
tayutaedomo <tayutaedomo@gmail.com>
2023-05-20 02:25:55 +0900

    Contract.sol: 実装を追加

--- contracts/Section04/practice/Contract.sol
+++ contracts/Section04/practice/Contract.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.17;
 
-/** 
+/**
  * @title Contract Typeを学ぼう
  * 全Contractはそれ自身の型を定義する
  * Address型に明示的に変換できる
@@ -11,57 +11,77 @@ pragma solidity ^0.8.17;
  */
 contract Contract {
     /// @dev constructorやfunctionにpayableを付与、宣言すると、コントラクトはETHを受け取れるようになる
-
+    constructor() payable {}
 
     /// @dev Addressコントラクトの型情報(type)から名前を取得
+    function getAddressName() public pure returns (string memory) {
+        return type(Address).name;
+    }
 
-    /** 
+    /**
      * @dev Addressコントラクトの型情報(type)から作成コードを取得
      * creationCodeはスマートコントラクトのコンストラクタロジックとコンストラクタパラメータを含む
      */
+    function getAddressCreationCode() public pure returns (bytes memory) {
+        return type(Address).creationCode;
+    }
 
-
-    /** 
+    /**
      * @dev Addressコントラクトの型情報(type)からランタイムコードを取得
      * runtimeCodeはオンチェーンに保存されるコード
      * このコードには、コントラクトのコンストラクタ・ロジックやコンストラクタ・パラメータは含まれない
      */
+    function getAddressRuntimeCode() public pure returns (bytes memory) {
+        return type(Address).runtimeCode;
+    }
 
     /// @dev thisは現コントラクト(Contract)を意味する
+    function getContractAddress() public view returns (address) {
+        return address(this);
+    }
 
     /// @dev 現コントラクトを破棄し、その資金を与えられたアドレスに送る。本当に削除されるのはトランザクション終了時。
+    function destruct() public {
+        selfdestruct(payable(msg.sender));
+    }
 
     /// @dev コントラクト型（MyContract c）のローカル変数を定義し、Addressコントラクトのファンクションを実行
-    
+    function callAddressFunction(address _addr) public view returns (uint256) {
+        Address addrContract = Address(_addr);
+        return addrContract.getBalance();
+    }
+
     /// @dev コントラクトのインスタンス化
     // 別途改めて扱う
- 
+    function callIAddressFunction() public returns (uint256) {
+        Address addrContract = new Address();
+        return addrContract.getBalance();
+    }
 }
 
-/** 
+/**
  * @title Address Typeを学ぼう
  */
 contract Address {
-    string public ownerName;
-
-    constructor(string memory ownerName_) {
-        ownerName = ownerName_;
-    }
-
     /// @dev msg.senderはクエリ/トランザクション元アカウントアドレスが入っているグローバル変数
-    address public fromAddr = msg.sender;
+    address public fromAddr;
+
+    constructor() {
+        fromAddr = msg.sender;
+    }
 
     /// @dev アカウントアドレス(EOA)の所有ETHを取得
     function getBalance() public view returns (uint256) {
         uint256 balance = fromAddr.balance;
         return balance;
     }
+
     /// @dev コントラクトのバイトコードを取得
     function getByteCode() public view returns (bytes memory) {
         return address(this).code;
     }
 
-    /** 
+    /**
      * @dev ETH送金 transfer/send/callの違い
      * transfer:宛先がコントラクトのアドレスである場合、
      * そのコード (Receive Ether Functionまたは Fallback Functionが存在すれば実行される
@@ -74,7 +94,7 @@ contract Address {
      *
      * call:ABIに準拠しないコントラクトとのインタフェースや、エンコーディングをより直接的に制御するために用意されている。
      * これらはすべて1バイトのメモリパラメータを取り、成功条件（bool）と返されたデータ（バイトメモリ）を返す
-     */    
+     */
 
     /// @dev 宛先アドレスにtransferでETHを移転(移転の場合send/callよりもtransferを使おう)
     function transfer(address payable to) public payable {
@@ -87,7 +107,9 @@ contract Address {
     }
 
     /// @dev 宛先アドレスにcallでETHを移転
-    function call(address payable to) public payable returns (bool, bytes memory) {
+    function call(
+        address payable to
+    ) public payable returns (bool, bytes memory) {
         return to.call{value: msg.value}("");
     }
 }
