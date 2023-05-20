34c9494d831427c87a764991ff13f185c336e283
tayutaedomo <tayutaedomo@gmail.com>
2023-05-20 18:44:37 +0900

    DataLocation.sol: 実装を追加

--- contracts/Section05/practice/DataLocation.sol
+++ contracts/Section05/practice/DataLocation.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.17;
 
-/** 
+/**
  * @title Data Location を学ぼう
  *
  * すべてのReference Types(参照型)には、それがどこに格納されるかについての「データロケーション」という指定が必要。
@@ -17,11 +17,54 @@ pragma solidity ^0.8.17;
  * storage, <=> memory,calldata : 値そのもののコピーを渡す
  * memory <=> memory : 値への参照のコピーを渡す
  * storage => local storage : 値への参照のコピーを渡す
- ＊ ※local storage : 関数内で定義された変数のこと
+ * ※local storage : 関数内で定義された変数のこと
  * storageへの他の割り当ては常に値そのもののコピーを渡す
  */
 contract DataLocation {
-
     // 状態変数はstorage
+    struct Character {
+        string name;
+        uint level;
+    }
+    Character[] public characters;
 
+    string public a;
+    string public b;
+    string public c = "ccc";
+
+    function setA() public view returns (string memory, string memory) {
+        // storage, <=> memory,calldata : 値そのもののコピーを渡す
+        string memory aa = a;
+        // a と aa はそれぞれ独立しているので、a は変更されない
+        aa = "hoge";
+
+        // 元の状態変数定義の初期設定の値を返す
+        return (a, aa);
+    }
+
+    function addCharacter(string memory name_, uint level_) public {
+        // memory <=> memory : 値への参照のコピーを渡す
+        Character memory character = Character(name_, level_);
+        Character memory characterChanged = character;
+
+        /*
+         * targetData <= charactor
+         *    ^
+         *    |
+         *  characterChanged
+         */
+        characterChanged.name = "aaa";
+
+        // 状態変数の新しい配列要素に追加
+        characters.push(character);
+    }
+
+    function setC() public returns (string memory) {
+        // storage => local storage : 値への参照のコピーを渡す
+        // ※local storage : 関数内で定義された変数のこと
+        string storage d = c;
+
+        c = "222";
+        return d;
+    }
 }
