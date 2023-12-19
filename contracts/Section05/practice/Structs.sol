3bbbf58d0012abd05e37264387ae7f29162122f1
tayutaedomo <tayutaedomo@gmail.com>
2023-12-19 23:48:20 +0900

    Lecture 91 is completed

--- contracts/Section05/practice/Structs.sol
+++ contracts/Section05/practice/Structs.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.17;
 
-/** 
+/**
  * @title Structs Type を学ぼう
  * Reference Typesに属する
  * Struct(構造体)の形で新しい型を定義できる、複数の変数をグループ化することができるカスタムタイプ
@@ -11,5 +11,22 @@ pragma solidity ^0.8.17;
  */
 contract Structs {
     // 構造体定義
+    struct Account {
+        uint no;
+        address addr;
+    }
 
+    Account public account =
+        Account(1, 0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199);
+
+    // Account public account =
+    //     Account({addr: 0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199, no: 1});
+
+    function getAccount() public view returns (uint) {
+        return account.no;
+    }
+
+    function setAccount() external {
+        account.addr = msg.sender;
+    }
 }
