16a6458443419a524a2258b7d51f314db91d20c1
tayutaedomo <tayutaedomo@gmail.com>
2023-12-30 20:11:48 +0900

    Lecture 112 is completed

--- contracts/Section06/practice/ABIEncodeDecodeFunction.sol
+++ contracts/Section06/practice/ABIEncodeDecodeFunction.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.17;
 
-/** 
+/**
  * @title ABI Encoding and Decoding Functions を学ぼう
  * コントラクトアプリケーションバイナリインターフェース（ABI）は、ブロックチェーンの外側からとコントラクト間の
  * 相互作用の両方において、イーサリアムエコシステム内のコントラクトと対話するための標準的な方法のこと
@@ -17,10 +17,29 @@ contract ABIEncodeDecodeFunction {
     //   https://docs.soliditylang.org/en/latest/050-breaking-changes.html#semantic-only-changes
 
     // encodeする。パディングあり
+    function encode(
+        string memory s1_,
+        string memory s2_
+    ) public pure returns (bytes memory) {
+        return abi.encode(s1_, s2_);
+    }
 
     // Solidity は非標準の packed モードをサポート。パディングなし
+    function encodePacked(
+        string memory s1_,
+        string memory s2_
+    ) public pure returns (bytes memory) {
+        return abi.encodePacked(s1_, s2_);
+    }
 
     // abi.encode()の結果をdecodeする
-
-
+    function decode(
+        bytes memory encodedData
+    ) public pure returns (string memory, string memory) {
+        (string memory s1, string memory s2) = abi.decode(
+            encodedData,
+            (string, string)
+        );
+        return (s1, s2);
+    }
 }
